8c0ce7d9988d3a901cca7ac86fe435093b4ca53e
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-18 12:51:35 +0530

    debug: last frame not working

--- solution.js
+++ solution.js
@@ -8,31 +8,47 @@ const getScore= (rolls)=> {
   let rollInd= 0;
   let frameStart= 0;
   for (let frame= 0; frame< 10; ++frame) {
+    console.log(frame, rollInd, frameStart, rolls[rollInd])
     if (rolls[frameStart]=== 10) {
       // strike
+      console.log('strike');
       frameScores.push(10 + rolls[rollInd+1]+rolls[rollInd+2]) // no need tto handle index range errors here 
     } else {
       // buffer : spare or open
       let buff= framePinLimit - rolls[frameStart];
-      rollInd= frameStart + 1;
-      if (rolls[rollInd]> buff) throw new Error('wrong input')
+      rollInd= ++frameStart;
+      if (rolls[rollInd]> buff) {
+        console.log(rolls[rollInd], buff)
+        throw new Error('wrong input')
+      }
       else if (rolls[rollInd]=== buff) {
         // spare
+        console.log('spare')
         frameScores.push(10 + rolls[rollInd+1])
       }
       else {
         // open
+        console.log('open')
         frameScores.push(rolls[frameStart] + rolls[rollInd])
       }
     }
 
     rollInd++;
     frameStart++;
+    console.log(frameScores)
   }
-  return frameScores
+  return frameScores.reduce((scr, curr)=> scr+curr);
 }
 
 const getBestScores= (games)=> {
   if (!Array.isArray(rolls) || games.some(game=> !Array.isArray(game)) ) throw new Error('Invalid argument')
   return Math.max(...games.map(rolls=> getScore(rolls)));
+}
+
+
+console.log(getScore([10, 5, 5, 9, 0]))
+
+module.exports= {
+  getBestScores,
+  getScore
 }
